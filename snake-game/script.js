d4dc968f7ec5b5ab4bf9d63dbf7cc954d93199ea
pagdrgn <andinityasasih28@gmail.com>
2022-03-13 23:58:03 +0700

    Mengganti ukuran panjang dan lebar snakeboard dan menjadikan hanya 1 ular yang terlihat

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,8 +1,9 @@
-const CELL_SIZE = 20;
-const CANVAS_SIZE = 400;
+const CELL_SIZE = 15;
+const CANVAS_WIDTH = 900;
+const CANVAS_HEIGHT = 420;
 const REDRAW_INTERVAL = 50;
-const WIDTH = CANVAS_SIZE / CELL_SIZE;
-const HEIGHT = CANVAS_SIZE / CELL_SIZE;
+const WIDTH = CANVAS_WIDTH / CELL_SIZE;
+const HEIGHT = CANVAS_HEIGHT / CELL_SIZE;
 const DIRECTION = {
     LEFT: 0,
     RIGHT: 1,
@@ -40,7 +41,6 @@ function initSnake(color) {
     }
 }
 let snake1 = initSnake("purple");
-let snake2 = initSnake("blue");
 
 let apple = {
     color: "red",
@@ -53,15 +53,10 @@ function drawCell(ctx, x, y, color) {
 }
 
 function drawScore(snake) {
-    let scoreCanvas;
-    if (snake.color == snake1.color) {
-        scoreCanvas = document.getElementById("score1Board");
-    } else {
-        scoreCanvas = document.getElementById("score2Board");
-    }
+    let scoreCanvas = document.getElementById("score1Board");
     let scoreCtx = scoreCanvas.getContext("2d");
 
-    scoreCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    scoreCtx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     scoreCtx.font = "30px Arial";
     scoreCtx.fillStyle = snake.color
     scoreCtx.fillText(snake.score, 10, scoreCanvas.scrollHeight / 2);
@@ -72,32 +67,27 @@ function draw() {
         let snakeCanvas = document.getElementById("snakeBoard");
         let ctx = snakeCanvas.getContext("2d");
 
-        ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         
         drawCell(ctx, snake1.head.x, snake1.head.y, snake1.color);
         for (let i = 1; i < snake1.body.length; i++) {
             drawCell(ctx, snake1.body[i].x, snake1.body[i].y, snake1.color);
         }
-        drawCell(ctx, snake2.head.x, snake2.head.y, snake2.color);
-        for (let i = 1; i < snake2.body.length; i++) {
-            drawCell(ctx, snake2.body[i].x, snake2.body[i].y, snake2.color);
-        }
         drawCell(ctx, apple.position.x, apple.position.y, apple.color);
 
         drawScore(snake1);
-        drawScore(snake2);
     }, REDRAW_INTERVAL);
 }
 
 function teleport(snake) {
     if (snake.head.x < 0) {
-        snake.head.x = CANVAS_SIZE / CELL_SIZE - 1;
+        snake.head.x = CANVAS_WIDTH / CELL_SIZE - 1;
     }
     if (snake.head.x >= WIDTH) {
         snake.head.x = 0;
     }
     if (snake.head.y < 0) {
-        snake.head.y = CANVAS_SIZE / CELL_SIZE - 1;
+        snake.head.y = CANVAS_HEIGHT / CELL_SIZE - 1;
     }
     if (snake.head.y >= HEIGHT) {
         snake.head.y = 0;
@@ -151,7 +141,6 @@ function checkCollision(snakes) {
     if (isCollide) {
         alert("Game over");
         snake1 = initSnake("purple");
-        snake2 = initSnake("blue");
     }
     return isCollide;
 }
@@ -172,7 +161,7 @@ function move(snake) {
             break;
     }
     moveBody(snake);
-    if (!checkCollision([snake1, snake2])) {
+    if (!checkCollision([snake1])) {
         setTimeout(function() {
             move(snake);
         }, MOVE_INTERVAL);
@@ -209,21 +198,10 @@ document.addEventListener("keydown", function (event) {
     } else if (event.key === "ArrowDown") {
         turn(snake1, DIRECTION.DOWN);
     }
-
-    if (event.key === "a") {
-        turn(snake2, DIRECTION.LEFT);
-    } else if (event.key === "d") {
-        turn(snake2, DIRECTION.RIGHT);
-    } else if (event.key === "w") {
-        turn(snake2, DIRECTION.UP);
-    } else if (event.key === "s") {
-        turn(snake2, DIRECTION.DOWN);
-    }
 })
 
 function initGame() {
     move(snake1);
-    move(snake2);
 }
 
 initGame();
