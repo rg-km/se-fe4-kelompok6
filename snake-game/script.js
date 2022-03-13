5adc20ed3dc050ec840346f0e2e7d0f4e8b086fc
dewisasikarani <l200190054@student.ums.ac.id>
2022-03-14 01:48:57 +0700

    Menambahkan kontrol android support

--- snake-game/script.js
+++ snake-game/script.js
@@ -467,6 +467,52 @@ document.addEventListener("keydown", function (event) {
     }
 })
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            turn(snake1, DIRECTION.LEFT); 
+        } else {
+            turn(snake1, DIRECTION.RIGHT);
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            turn(snake1, DIRECTION.UP);
+        } else { 
+            turn(snake1, DIRECTION.DOWN);
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
 function initGame() {
     move(snake1);
 }
